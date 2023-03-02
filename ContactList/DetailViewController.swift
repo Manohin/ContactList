240d97b9203d96dc9fed55361ef236818bbe4604
Manohin <111537419+Manohin@users.noreply.github.com>
2023-03-02 12:44:33 +0300

    Add detail screen

---
+++ ContactList/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ContactList
+//
+//  Created by Alexey Manokhin on 02.03.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    @IBOutlet var phoneNumberLabel: UILabel!
+    @IBOutlet var emailAddressLabel: UILabel!
+    
+    
+    var person: Person!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = person.fullName
+        phoneNumberLabel.text = "Телефон: " + person.phoneNumber
+        emailAddressLabel.text = "E-mail: " + person.emailAddress
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
